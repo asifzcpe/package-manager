2cefb59503d432aa6c77fc1fff8f10196149c896
asifzcpe <rahmanasif05004@gmail.com>
2020-02-11 01:35:48 +0600

    added even source and stream response

--- src/Routes.php
+++ src/Routes.php
@@ -1,25 +1,57 @@
 <?php
 
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 Route::get('/package-manager','Asif\PackageManager\PackageManagerController@index');
 Route::get('/run',function (){
+
+//
+//    $process=new Process('ping -c 4 google.com');
+//    $process->run(function ($type, $buffer) {
+//        if (Process::ERR === $type) {
+//            echo 'ERR > '.$buffer;
+//        } else {
+//            echo 'OUT > '.$buffer;
+//        }
+//    });
 //    $last_line = system('composer require asif/laravel-moduler', $retval);
 //    echo '<pre>';
 //    passthru('composer dumpautoload');
 //    echo '</pre>';
+//        echo base_path();
+//     while (@ ob_end_flush()); // end all output buffers if any
+//     // composer require asif/laravel-moduler 2>&1
+//
+//     $proc = popen("composer require -d ".base_path()." asif/laravel-moduler 2>&1", 'r');
+//     echo '<pre>';
+//     while (!feof($proc))
+//     {
+//         echo fread($proc, 409000006);
+//
+//         @ flush();
+//     }
+//     echo '</pre>';
+//    $proc = popen("composer dumpautoload 2>&1", 'r');
+    $proc = popen("composer require -d ".base_path()." asif/laravel-moduler 2>&1", 'r');
+    $response = new StreamedResponse(function() use($proc){
+        while(!feof($proc)) {
+            echo 'data: ' . fread($proc, 409000006) . "\n\n";
 
-    while (@ ob_end_flush()); // end all output buffers if any
+            flush();
+            usleep(200000);
+        }
+        echo "event: process_ended\n";
+        echo "data: ddddddd \n\n";
 
-    $proc = popen("composer require asif/laravel-moduler 2>&1", 'r');
-    echo '<pre>';
-    while (!feof($proc))
-    {
-        echo fread($proc, 409000006);
-        @ flush();
-    }
-    echo '</pre>';
+    });
 
-//    echo "composer: " . shell_exec('cd .. ; composer install --no-interaction --no-dev --prefer-dist 2>&1');
+    $response->headers->set('Content-Type', 'text/event-stream');
+    $response->headers->set('X-Accel-Buffering', 'no');
+    $response->headers->set('Cach-Control', 'no-cache');
+    return $response;
 
+//   echo "composer: " . shell_exec('composer dumpautoload 2>&1');
+//        echo base_path();
 //    echo exec('composer dumpautoload');
 
 });
--- src/resources/js/components/ExampleComponent.vue
+++ src/resources/js/components/ExampleComponent.vue
@@ -10,8 +10,11 @@
             <div class="col-md-3">
                 <input type="button" @click="next(pageLink)" class="btn btn-info" value="Previous"/>
                 <input type="button" @click="next(pageLink)" class="btn btn-info" value="Next"/>
+                <button type="button" class="btn btn-success btn-sm" data-toggle="modal" @click="install" data-target="#exampleModal">Install</button>
+                <button type="button" class="btn btn-success btn-sm" data-toggle="modal" @click="stop" data-target="#exampleModal">Stop</button>
             </div>
         </div>
+        <br>
         <div class="tableFixHead">
             <table class="table table-sm">
                 <thead>
@@ -40,7 +43,26 @@
                 </tr>
                 </tbody>
             </table>
-
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -54,6 +76,7 @@
     .tableFixHead thead th {
         position: sticky;
         top: 0;
+        z-index: 9;
     }
 
     /* Just common table stuff. Really. */
@@ -78,6 +101,7 @@
                 packages: [],
                 totalResults: 0,
                 pageLink: '',
+                eventSource:null,
             }
         },
         created() {
@@ -100,6 +124,23 @@
                     this.totalResults = data.total;
                     this.pageLink = data.next;
                 })
+            },
+            install(){
+                // axios.get('http://localhost:1000/run').then((response) => {
+                //     console.log(response);
+                // })
+                this.eventSource = new EventSource('http://localhost:9000/run');
+
+                this.eventSource.onmessage = function(e) {
+                    console.log(e.data);
+                }
+
+                this.eventSource.addEventListener('process_ended',(e)=>{
+                    this.eventSource.close();
+                });
+            },
+            stop(){
+
             }
         }
     }
